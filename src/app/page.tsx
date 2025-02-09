edc3ef8f3991ac11346d59d852c9ba53d18d2dcc
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-02-09 19:04:38 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client"; 
 import Footer from "./Footer";
 import Hero from "./Hero";
-import Map from "./Map";
-import Navbar from "./Navbar";
 import Services from "./Services";
+import Navbar from "./Navbar";
+import dynamic from "next/dynamic";
 
+// Dynamically import the Map component without SSR
+const Map = dynamic(() => import("./Map"), {
+  ssr: false, // This disables SSR for the Map component
+});
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-">
-      <Navbar/>
-      <Hero/>
-      <Services/>
-      <Map/>
-      <Footer/>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Map />
+      <Footer />
     </div>
   );
 }

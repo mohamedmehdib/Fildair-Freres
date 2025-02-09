83177cc1a13e792391744188246897864266039f
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-02-09 19:00:08 +0100

    first commit

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
 
   return (
     <div className="md:h-[80vh] flex md:flex-row flex-col-reverse items-center md:justify-around justify-center bg-[#274e9d] relative overflow-hidden pt-10 md:py-10">
-      <Image src="/hero.jpeg" alt="Hero" width={1000} height={1000} style={{ transform: `translateY(${scrollY * 3}px)` }}/>
+      <Image priority src="/hero.jpeg" alt="Hero" width={1000} height={1000} style={{ transform: `translateY(${scrollY * 3}px)` }}/>
     </div>
   );
 }
--- src/app/Slider.tsx
+++ src/app/Slider.tsx
@@ -1,31 +1,31 @@
-"use client";
+// "use client";
 
-import Image from "next/image";
-import React from "react";
-import Marquee from "react-fast-marquee";
+// import Image from "next/image";
+// import React from "react";
+// import Marquee from "react-fast-marquee";
 
-const App: React.FC = () => {
-  const logos = [
-    <Image src="/pool.svg" alt="Logo 1" className="h-16" width={500} height={500}/>,
-  ];
+// const App: React.FC = () => {
+//   const logos = [
+//     <Image src="/pool.svg" alt="Logo 1" className="h-16" width={500} height={500}/>,
+//   ];
 
-  return (
-    <div className="bg-blue-700">
-      <Marquee
-        speed={50}
-        gradient={false}
-        direction="left"
-        loop={0}
-        key={1}
-      >
-        {[...logos, ...logos, ...logos, ...logos, ...logos].map((logo, index) => (
-          <div key={index} className="flex-shrink-0 px-5">
-            {logo}
-          </div>
-        ))}
-      </Marquee>
-    </div>
-  );
-};
+//   return (
+//     <div className="bg-blue-700">
+//       <Marquee
+//         speed={50}
+//         gradient={false}
+//         direction="left"
+//         loop={0}
+//         key={1}
+//       >
+//         {[...logos, ...logos, ...logos, ...logos, ...logos].map((logo, index) => (
+//           <div key={index} className="flex-shrink-0 px-5">
+//             {logo}
+//           </div>
+//         ))}
+//       </Marquee>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;

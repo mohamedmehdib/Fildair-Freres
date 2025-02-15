d2038b73cf579b067564e0979e5814b0a94241dd
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-02-15 21:55:26 +0100

    first commit

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -23,8 +23,8 @@ export default function Hero() {
   }
 
   return (
-    <div id="accueil" className="md:h-[80vh] flex md:flex-row flex-col-reverse items-center md:justify-around justify-center bg-[#274e9d] relative overflow-hidden py-16 pt-24 md:py-10">
-      <Image priority src="/heroo.jpeg" alt="Hero" className="w-80 rounded-lg" width={1000} height={1000} style={{ transform: `translateY(${scrollY * 3}px)` }}/>
+    <div id="accueil" className="md:h-[80vh] flex md:flex-row flex-col-reverse items-center md:justify-around justify-center bg-[#274e9d] relative overflow-hidden py-16 md:py-10">
+      <Image priority src="/hero.jpeg" alt="Hero" width={1000} height={1000} style={{ transform: `translateY(${scrollY * 3}px)` }}/>
     </div>
   );
 }

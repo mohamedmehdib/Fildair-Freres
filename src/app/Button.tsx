500367d8af64a76d5e4f5e357e46cbd6747fc852
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-02-15 19:14:27 +0100

    first commit

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -3,15 +3,16 @@ import Link from 'next/link';
 
 export default function WhatsAppButton() {
   return (
-    <div className="fixed bottom-[50vh] left- z-50">
+    <div className="fixed z-50 bottom-[50vh]">
       <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
       <Link
         href="https://wa.me/28700199" // Replace with your WhatsApp number
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center justify-center w-14 h-14 bg-green-500 rounded-e-full shadow-lg hover:bg-green-600 transition-all duration-300"
+        className="flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 bg-green-500 rounded-e-full shadow-lg hover:bg-green-600 transition-all duration-300"
+        aria-label="Contact us on WhatsApp"
       >
-        <i className="uil uil-whatsapp text-3xl text-white"></i>
+        <i className="uil uil-whatsapp text-2xl sm:text-3xl text-white"></i>
       </Link>
     </div>
   );

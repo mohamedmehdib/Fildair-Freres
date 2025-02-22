5b164de333dce879fa48e322de3515b53fa5488a
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-02-22 19:05:15 +0100

    first commit

--- src/utils/loadTranslations.ts
+++ src/utils/loadTranslations.ts
@@ -2,22 +2,28 @@ import en from '../locales/en.json';
 import fr from '../locales/fr.json';
 import es from '../locales/es.json';
 
-// Define a flexible Translations type
+// Define the Translations type
 type Translations = {
-  [key: string]: {
-    [key: string]: string | { [key: string]: string | { [key: string]: string } };
+  about: {
+    about_us: string;
+    heading: string;
+    description: string;
+    completed_projects: string;
+    team_members: string;
+    satisfied_clients: string;
   };
+  // Add other sections as needed
 };
 
 // Create the translations object with the correct type
-const translations: Translations = {
-  en,
-  fr,
-  es,
+const translations: { [key: string]: Translations } = {
+  en: en as Translations,
+  fr: fr as Translations,
+  es: es as Translations,
 };
 
 // Function to load translations based on the user's language
-export const loadTranslations = (language: string) => {
+export const loadTranslations = (language: string): Translations => {
   const languageCode = language.split('-')[0];
   return translations[languageCode] || translations['fr'];
 };

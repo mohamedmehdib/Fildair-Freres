e1ae1c5a64291d5be8e0ee9c097a65f69f68aa7c
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-02-22 19:15:47 +0100

    first commit

--- src/utils/loadTranslations.ts
+++ src/utils/loadTranslations.ts
@@ -1,12 +1,14 @@
 import en from '../locales/en.json';
 import fr from '../locales/fr.json';
 
-const translations: { [key: string]: any } = {
+type Translations = Record<string, typeof en>; // Supposant que les fichiers de traduction ont la même structure
+
+const translations: Translations = {
   en,
   fr,
 };
 
 export const loadTranslations = (language: string) => {
   const languageCode = language.split('-')[0]; // Extract the base language code (e.g., 'en' from 'en-US')
-  return translations[languageCode] || translations['fr']; // Fallback to English if the language is not supported
+  return translations[languageCode] || translations['fr']; // Fallback to French if the language is not supported
 };
